550c1c620b56dacbb948bdc882f859af5da1f968
Singhal <2464017@cognizant.com>
2026-02-18 11:31:08 +0530

    config file

---
+++ src/main/java/baseclass/BaseClass.java
@@ -0,0 +1,35 @@
+package baseclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import utils.Utils;
+
+import java.time.Duration;
+
+public class BaseClass {
+    public WebDriver driver;
+    @BeforeTest
+    public void setUp() throws Exception {
+        if(Utils.fetchPropertyValue("browser").equals("chrome")) {
+            driver = new ChromeDriver();
+        }else if(Utils.fetchPropertyValue("browser").equals("firefox")){
+            driver = new FirefoxDriver();
+        }
+        else if(Utils.fetchPropertyValue("browser").equals("edge")){
+            driver = new EdgeDriver();
+        }
+
+        driver.get(Utils.fetchPropertyValue("URL").toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+        driver.quit();
+    }
+}
